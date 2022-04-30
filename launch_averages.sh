f85a8aa56b676d75a489b1e750a141b81697f48d
Andrea Tirelli <atirelli@sissa.it>
2022-04-30 17:19:43 +0200

    updated launch files

--- launch_averages.sh
+++ launch_averages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-
-SPHOME/bin/spark-submit --packages io.delta:delta-core_2.12:1.2.1 --py-files packages.zip  --files  configs/etl_config.json \
+PYSPARKHOME=/u/a/atirelli/.miniconda3/envs/camlin/
+$PYSPARKHOME/bin/spark-submit --packages io.delta:delta-core_2.12:1.2.1 --py-files packages.zip  --files  configs/etl_config.json \
 --conf "spark.sql.extensions=io.delta.sql.DeltaSparkSessionExtension" \
 			    --conf "spark.sql.catalog.spark_catalog=org.apache.spark.sql.delta.catalog.DeltaCatalog" \
 			    jobs/etl_job_averages.py
